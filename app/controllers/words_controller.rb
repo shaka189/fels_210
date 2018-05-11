d71fd5c085eef32bf0a9a0c2c2a219f1732f4b30
Viettien <viettienn97@gmail.com>
2018-05-11 09:32:19 +0700

    add gem devise and define model

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,7 @@
+class WordsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "words/new"
+  get "words/show"
+  get "categories/new"
+  get "categories/show"
+  get "activities/new"
+  get "activities/show"
+  get "lessons/new"
+  get "lessons/show"
+  devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
